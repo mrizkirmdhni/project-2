a11a67c171ec42e3fcb77c452a83e1cdab2521eb
mrizkirmdhni <71094694+mrizkirmdhni@users.noreply.github.com>
2020-10-28 22:20:02 +0700

    Penambahan E-Commerce

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/layout', function () {
+    return view('admin.layout');
+});
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/experiance', function () {
+    return view('experiance');
+});
+Route::get('/shop', function () {
+    return view('shop');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/admin', function () {
+    return view('admin.admin');
+});
+Route::get('/checkout2', function () {
+    return view('checkout2');
+});
+Route::get('/contact2', function () {
+    return view('contact2');
+});
+Route::get('/experiance2', function () {
+    return view('experiance2');
+});
+Route::get('/shop2', function () {
+    return view('shop2');
+});
+Route::get('/login2', function () {
+    return view('login2');
+});
